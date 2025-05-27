9729bb116845470881a96e4002e87bc77884ff24
Sergio Sala Sastre <sergio.sala@inetum.com>
2025-05-27 16:56:57 +0200

    primer planteamiento back con error en getAll

---
+++ FullStack/src/main/java/com/fullStackHexagonal/fullstackHexagonal/FullstackHexagonalApplication.java
@@ -0,0 +1,13 @@
+package com.fullStackHexagonal.fullstackHexagonal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullstackHexagonalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullstackHexagonalApplication.class, args);
+	}
+
+}
---
+++ FullStack/src/test/java/com/fullStackHexagonal/fullstackHexagonal/FullstackHexagonalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fullStackHexagonal.fullstackHexagonal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullstackHexagonalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
